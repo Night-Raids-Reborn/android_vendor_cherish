7034898f1c00c7505ce723981aeed4a0e5bc5a03
Neel Chauhan <neel@neelc.org>
2022-07-21 09:30:48 -0700

    kernel.mk: Allow building external kernel modules
    Here, we introduce the TARGET_EXTERNAL_KERNEL_MODULES variable
    for the list of external modules.
    
    Change-Id: I1f8b7a48e66bb0c9efbc396692e5a7159f0b835c
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -38,7 +38,12 @@
 #   TARGET_KERNEL_DTBO                 = Name of the kernel Makefile target that
 #                                        generates dtbo.img. Defaults to dtbo.img
 #   TARGET_KERNEL_DTB                  = Name of the kernel Makefile target that
-#                                        generates the *.dtb targets. Defaults to dtbs
+#                                        generates the *.dtb targets. Defaults to dtb
+#
+#   TARGET_KERNEL_EXT_MODULE_ROOT      = Optional, the external modules root directory
+#                                          Defaults to empty
+#   TARGET_KERNEL_EXT_MODULES          = Optional, the external modules we are
+#                                          building. Defaults to empty
 #   KERNEL_TOOLCHAIN_PREFIX            = Overrides TARGET_KERNEL_CROSS_COMPILE_PREFIX,
 #                                          Set this var in shell to override
 #                                          toolchain specified in BoardConfig.mk
@@ -185,6 +190,10 @@ endif
 
 # Set the default dtb target
 TARGET_KERNEL_DTB ?= dtbs
+
+# Set no external modules by default
+TARGET_KERNEL_EXT_MODULE_ROOT ?=
+TARGET_KERNEL_EXT_MODULES ?=
 
 # Set use the full path to the make command
 KERNEL_MAKE_CMD := $(BUILD_TOP)/prebuilts/build-tools/$(HOST_PREBUILT_TAG)/bin/make
